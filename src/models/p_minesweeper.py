d0876bae2b7d9b72365053680d26475b439ba236
Leo-vF <vonfoerster.leo@gmail.com>
2020-12-29 14:52:14 +0100

    updated models

--- src/models/p_minesweeper.py
+++ src/models/p_minesweeper.py
@@ -1,10 +1,17 @@
 from tortoise.models import Model
 from tortoise import fields
 
-from .spot import spot
-
 
 class p_minesweeper(Model):
-    id = fields.IntField(pk=True)
-    code = fields.IntField()
-    field = fields.foreignKeyField(spot, on_delete=fields.CASCADE)
+    """The Database model for a minesweeper game.
+
+    Attributes / fields:
+     - id [UUIDField] : Is the primary key for identifying a game
+     - code [IntField] : The Code players need to enter to    join a game
+     - field [jsonField] : json representation of the entire field with mines and field values
+     - open_field [jsonField] : json representation of all the fields that have been opened/marked by players
+    """
+    id = fields.UUIDField(pk=True)
+    code = fields.IntField()  # the code to enter the same game
+    field = fields.jsonField()  # the entire field that is saved after correct generation
+    open_field = fields.jsonField()  # all the opened and marked fields

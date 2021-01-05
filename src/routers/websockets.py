5a31013b528c310ca4092628929424fec76c182e
Leo-vF <vonfoerster.leo@gmail.com>
2021-01-05 22:17:01 +0100

    fixed type error in websockets

--- src/routers/websockets.py
+++ src/routers/websockets.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, WebSocket
 from fastapi.responses import HTMLResponse
-from ..models.db import db_minesweeper
+from ..models.db import db_minesweeper, minesweeper_pydantic
 
 router = APIRouter(prefix="/ws", tags=["Websocket"])
 
@@ -49,22 +49,24 @@ async def ws_create(websocket: WebSocket):
     await websocket.accept()
     data = await websocket.receive_json()
 
-    if data["n_cols"] <= 3:
+    if int(data["n_cols"]) <= 3:
         await websocket.send_json({"error": "Number of colums is too small"})
-    elif data["n_cols"] > 60:
+    elif int(data["n_cols"]) > 60:
         await websocket.send_json({"error": "Number of columns must be at or below 60"})
-    elif data["n_rows"] <= 3:
+    elif int(data["n_rows"]) <= 3:
         await websocket.send_json({"error": "Number of rows is too small"})
-    elif data["n_rows"] > 60:
+    elif int(data["n_rows"]) > 60:
         await websocket.send_json({"error": "Number of rows must be at or below 60"})
-    elif data["n_mines"] < 1:
+    elif int(data["n_mines"]) < 1:
         await websocket.send_json({"error": "Number of mines too small"})
-    elif data["m_mines"] >= data["n_rows"]*data["n_cols"]:
+    elif int(data["m_mines"]) >= data["n_rows"]*data["n_cols"]:
         await websocket.send_json({"error": "Number of mines too big"})
     else:
         try:
-            ms = await db_minesweeper.create(**data)
-            websocket.send_json(ms)
+            await db_minesweeper.create(**data)
+            ms = await minesweeper_pydantic.from_queryset_single(db_minesweeper.filter(code=int(data["code"])))
+            ms = ms.dict()
+            websocket.send_json(ms["id"])
         except Exception as e:
             await websocket.send_json(e)
 

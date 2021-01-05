4f7dae4492e147c1ca2f06ded0ae22f8bf8e860e
Leo-vF <vonfoerster.leo@gmail.com>
2021-01-05 20:21:33 +0100

    websocket test

---
+++ src/routers/websockets.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter, WebSocket
+
+router = APIRouter(prefix="/ws", tags=["Websocket"])
+
+
+@router.websocket()
+async def websocket_test(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Message text was: {data}")

7fda5887360e7f4eb51e37e50c080bccf99cbdf2
Leo-vF <vonfoerster.leo@gmail.com>
2021-01-05 11:08:05 +0100

    fix db issues in set_Flag; ifs in open for flags

--- src/routers/field.py
+++ src/routers/field.py
@@ -2,8 +2,6 @@ from fastapi import APIRouter
 from numpy import ravel
 from typing import List
 
-from numpy.core.fromnumeric import size
-
 from ..minesweeper import Minesweeper
 from ..ms_models import db_minesweeper, db_spot, minesweeperIn_pydantic, spot_pydantic, minesweeper_pydantic
 
@@ -77,21 +75,21 @@ async def open(code: int, col: int, row: int, double_click: bool = False):
                 (col-1, row+1), (col, row+1), (col+1, row+1)
             ]
 
-            if spot["opened"] == False:
+            if spot["opened"] == False and spot["flagged"] == False:
                 await update_opened(code, col, row)
                 OPENED.append(spot)
                 for nb in neighbors:
                     if 0 <= nb[0] < n_rows and 0 <= nb[1] < n_cols:
                         nb = await spot_pydantic.from_queryset_single(db_spot.get(code=code, col=nb[0], row=nb[1]))
                         nb = nb.dict()
-                        if nb["opened"] == False:
+                        if nb["opened"] == False and nb["flagged"] == False:
                             await open_zeros(nb, code, size)
 
     spot = await spot_pydantic.from_queryset_single(
         db_spot.get(code=code, col=col, row=row))
     spot_dict = spot.dict()
 
-    if spot_dict["mine"] == True:
+    if spot_dict["mine"] == True and spot_dict["flagged"] == False:
         # TODO define better return value for frontend
         return {"game_status": "lost"}
 
@@ -107,8 +105,8 @@ async def open(code: int, col: int, row: int, double_click: bool = False):
 
 @router.put("/set-flag/{code}-{col}-{row}")
 async def set_Flag(code: int, col: int, row: int):
-    db_spot_obj = await db_spot.filter(code=code, col=col, row=row)
-    spot = await spot_pydantic.from_queryset_single(db_spot_obj)
+    spot = await spot_pydantic.from_queryset_single(
+        db_spot.get(code=code, col=col, row=row))
     spot = spot.dict()
 
     if spot["opened"] == True:
@@ -118,6 +116,6 @@ async def set_Flag(code: int, col: int, row: int):
         # TODO define better return value for frontend
         return {"status": "Spot is already flagged"}
     else:
-        await db_spot_obj.update(flagged=True)
+        await db_spot.filter(code=code, col=col, row=row).update(flagged=True)
         # TODO define better return value for frontend
         return {"status": "Flagged Successfully"}

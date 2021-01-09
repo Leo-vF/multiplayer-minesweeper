fa56567131773778b8bf4c4f7b0f2b77079ad6ab
Leo-vF <vonfoerster.leo@gmail.com>
2021-01-09 15:21:05 +0100

    broadcast n_mines on mine placement

--- src/routers/websockets.py
+++ src/routers/websockets.py
@@ -111,6 +111,7 @@ async def ws_open(websocket: WebSocket, code: int):
                                       "code": ms_dict["code"], "flagged": False}
                     for spot in ravel(ms.field):
                         db_sp_obj = await db_spot.create(**{**spot.get_db_attribs(), **default_values})
+                    manager.broadcast({"n_mines": ms_dict["n_mines"]})
 
                 opened = await open(code, int(data["col"]), int(data["row"]))
                 if type(opened) == list:
